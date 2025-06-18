cbc235c6eb3c5536e3762680ba3e373a6952d73b
Kasi Reddy Srikanth <Srikanthkr@pyramidconsultinginc.com>
2025-06-18 12:51:46 +0530

    pom changes

--- tests/TimeSheet.spec.js
+++ tests/TimeSheet.spec.js
@@ -1,174 +1,49 @@
 
-import {test,expect} from '@playwright/test';
-import { LoginPage } from './Pages/loginpage';
-import {MyTimesheetHomePage} from './Pages/timesheetHomePage';
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pageObjects/LoginPage';
+import { TimesheetPage } from '../pageObjects/TimesheetPage';
+import testData from '../testdata/timesheetdata.json';
+import { getCurrentWeekMonday, getCurrentWeekSunday, getPreviousDay } from '../utils/datehelper';
+
+test('PCORE My Timesheet ', async ({ page }) => {
+
+    // LOGIN TO PCORE PORTAL  
+    const loginObj = new LoginPage(page);
+    await loginObj.goTOLoginPage();
+    await loginObj.login(testData.userName, testData.password);
+
+    //Navigate to MyTimesheet page
+    const myTimeSheet = new TimesheetPage(page);
+    await myTimeSheet.navigateToMyTimesheetLandingPage();
+
+    //Verify naviagate buttons for timesheets
+    await myTimeSheet.navigateBetweenTimeSheets();
 
 
-test('PCore',async({page})=>{
+    // Get current week Monday date
+    const monday = await getCurrentWeekMonday();
 
+    // Get current week Monday date
+    const sunday = await getCurrentWeekSunday();
 
+    // Get previous date based on user passed date
+    const previousDate = await getPreviousDay(monday);
 
-// LOGIN TO PCORE PORTAL  
-const loginObj= new LoginPage(page);
-await loginObj.goTOLoginPage();
-await loginObj.login('srikanth.kasireddy','Reddy@123');
+    //Verify timesheet dates and last submitted date
+    await myTimeSheet.verifyTimeSheetDates(monday, sunday, previousDate);
 
-//Navigate to MyTimesheet page
-const myTimeSheet= new MyTimesheetHomePage(page);
-await myTimeSheet.navigateToMyTimesheetLandingPage();
+    // ADD new row to select and enter project hours
+    await myTimeSheet.addProjectDetails(testData.projectGroup, testData.projectActivity);
 
+    //ADD Hours 
+    const timesheetHours = await myTimeSheet.addTimesheetHours();
 
-const f=await page.frameLocator("//frame[@name='main']");
+    // ADD Remarks for the hours
+    await myTimeSheet.addRemarksToHoursFilled(testData.remarks);
 
-//Verify naviagate buttons for timesheets
-await myTimeSheet.navigateBetweenTimeSheets();
+    //Submit Timesheet
+    await myTimeSheet.submitTimeSheet(timesheetHours);
 
-
-
-//await framez2.locator("//p[text()='Timesheet saved successfully']").nth(2).isVisible();
-
-
-
-//Verify timesheet dates and last submitted date
-await myTimeSheet.verifyTimeSheetDates();
-
-// ADD Remarks for the hours
-await myTimeSheet.addRemarksToHoursFilled('project hours added');
-
-// ADD new row to select and enter project hours
-await myTimeSheet.addProjectDetails('Quality','QA & Testing');
-
-//ADD Hours and Submit sheet
-await myTimeSheet.addHoursAndSubmitTimeSheet();
-
-/*
-const frame2= await page.frameLocator("//frame[@name='main']").locator("//select[@id='selectedHour']");
-//const minutes= await page.frameLocator("//frame[@name='main']").locator("//select[@id='slectedMinute']");
-
-
-
-
-
-
-
-
-for(let i=0;i<5;i++)
-{
-    if(await framez2.locator("//button[@id='btnNext332']").nth(i).isVisible())
-    {
-        await framez2.locator("//button[@id='btnNext332']").nth(i).click();
-        await framez2.locator("//button[@title='Click to add new row']").click();
-        await page.on('dialog', async dialog=>{
- 
-    //expect (dialog.type()).toContain('alert');
-    //expect (dialog.message()).toContain('Ticket List')
-    //await dialog.page().locator("//div[@class='modal-content']//a[@class='goToEdit']").click();
-   
-    await dialog.accept();
-
-})
-
- await page.frameLocator("//frame[@name='main']").locator ("//input[@title='Click to view ticket options']").click();
-await page.frameLocator("//frame[@name='main']").locator("//a[@class='goToEdit']").click(); 
-await framez2.locator("//select[@id='selectedGroup']").nth(2).selectOption('Quality');
-await framez2.locator("//select[@id='selectedActivity']").nth(2).selectOption('QA & Testing');
-        break;
-    }
-    else{
-         await framez2.locator("//button[@title='Click to add new row']").click();
-         await page.on('dialog', async dialog=>{
- 
-    //expect (dialog.type()).toContain('alert');
-    //expect (dialog.message()).toContain('Ticket List')
-    //await dialog.page().locator("//div[@class='modal-content']//a[@class='goToEdit']").click();
-   
-    await dialog.accept();
-
-})
-
- await page.frameLocator("//frame[@name='main']").locator ("//input[@title='Click to view ticket options']").click();
-await page.frameLocator("//frame[@name='main']").locator("//a[@class='goToEdit']").click(); 
-await framez2.locator("//select[@id='selectedGroup']").nth(2).selectOption('Quality');
-await framez2.locator("//select[@id='selectedActivity']").nth(2).selectOption('QA & Testing');
-          break;
-    }
-    }
-} 
-*/
-
-/*
-
-//Enter Hours
-
-if(await frame2.nth(14).isEnabled())
-{
-
-  await frame2.nth(14).selectOption('8');
-}
-
-if(await frame2.nth(15).isEnabled())
-{
-
-  await frame2.nth(15).selectOption('8');
-}
-
-if(await frame2.nth(16).isEnabled())
-{
- 
-  await frame2.nth(16).selectOption('8');
-}
-if(await frame2.nth(17).isEnabled())
-{
-
-  await frame2.nth(17).selectOption('8');
-}
-
-if(await frame2.nth(18).isEnabled())
-{
- 
-  await frame2.nth(18).selectOption('8');
-}
-
-let totalhours=0;
-for(let i=0;i<21;i++)
-{
-
-  totalhours=totalhours +  parseFloat(await frame2.nth(i).inputValue());
-
-}
-
-
-
-
-const tothrs=await framez2.locator("//div[@class='col-1 timeColumn rowheight totalColumn weekTotalLow']").nth(0).textContent();
-
-//Save and Submit time sheet if hours equals to 40 
-
-if (toString(tothrs)==toString(totalhours) && totalhours==40)
-{
-await framez2.locator("//button[@title='Click to save timesheet']").click();
-await framez2.locator("//p[text()='Timesheet saved successfully']").isVisible();
-}
-else
-{
-
-  console.log("total time mismatch  ",totalhours);
-  //console.log("total   ",tothrs);
-}
-
-if (totalhours==40)
-{
-    await expect(await framez2.locator("#submit")).toBeEnabled();
-    await expect(await framez2.locator("#submit")).click();
-}
-else
-{
-    await expect(await framez2.locator("#submit")).toBeDisabled();
-      
-}
-
-*/
-
-await page.waitForTimeout(3000);
+    await page.waitForTimeout(3000);
 
 })
